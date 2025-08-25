e0f81a68e30251a23de794f7bb0cba67a21f7b10
Захаров Даниил Сергеевич <zaxarov_ds@promarsenal.ru>
2025-08-25 11:39:49 +0500

    Патрон

---
+++ Push.bat
@@ -0,0 +1,17 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+
+set /p msg=Введите название коммита: 
+
+if "%msg%"=="" (
+    echo Вы не ввели название коммита.
+    pause
+    exit /b
+)
+
+git add index.html
+git commit -m "!msg!"
+git push
+
+pause
